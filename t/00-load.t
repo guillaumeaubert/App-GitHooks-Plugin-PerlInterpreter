guillaumeaubert/App-GitHooks-Plugin-PerlCompile

Add code and tests.

t/00-load.t
@@ -1,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 1;
+
+BEGIN
+{
+    use_ok( 'App::GitHooks::Plugin::PerlCompile' );
+}
+
+diag( "Testing App::GitHooks::Plugin::PerlCompile $App::GitHooks::Plugin::PerlCompile::VERSION, Perl $], $^X" );

t/03-get_file_check_description.t
@@ -1,0 +1,31 @@
+#!perl
+
+use strict;
+use warnings;
+
+use App::GitHooks::Plugin::PerlCompile;
+use Test::Exception;
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 3;
+use Test::Type;
+
+
+can_ok(
+	'App::GitHooks::Plugin::PerlCompile',
+	'get_file_check_description',
+);
+
+my $file_check_description;
+lives_ok(
+	sub
+	{
+		 $file_check_description = App::GitHooks::Plugin::PerlCompile->get_file_check_description();
+	},
+	'Retrieve the description.',
+);
+
+ok_string(
+	$file_check_description,
+	name        => 'The description',
+	allow_empty => 0,
+);

t/05-get_file_pattern.t
@@ -1,0 +1,28 @@
+#!/usr/bin/env perl
+
+=head1 DESCRIPTION
+
+Verify that the get_file_pattern() subroutine returns a pattern to match file names
+against.
+
+=cut
+
+use strict;
+use warnings;
+
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 2;
+use Test::Type;
+
+use App::GitHooks::Plugin::PerlCompile;
+
+
+can_ok(
+	'App::GitHooks::Plugin::PerlCompile',
+	'get_file_pattern',
+);
+
+ok_regex(
+	App::GitHooks::Plugin::PerlCompile->get_file_pattern(),
+	name => 'The return value of get_file_pattern()',
+);

guillaumeaubert/App-GitHooks-Plugin-PerlInterpreter

t/00-load.t
@@ -1,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 1;
+
+BEGIN
+{
+    use_ok( 'App::GitHooks::Plugin::PerlInterpreter' );
+}
+
+diag( "Testing App::GitHooks::Plugin::PerlInterpreter $App::GitHooks::Plugin::PerlInterpreter::VERSION, Perl $], $^X" );

t/03-get_file_check_description.t
@@ -1,0 +1,31 @@
+#!perl
+
+use strict;
+use warnings;
+
+use App::GitHooks::Plugin::PerlInterpreter;
+use Test::Exception;
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 3;
+use Test::Type;
+
+
+can_ok(
+	'App::GitHooks::Plugin::PerlInterpreter',
+	'get_file_check_description',
+);
+
+my $file_check_description;
+lives_ok(
+	sub
+	{
+		 $file_check_description = App::GitHooks::Plugin::PerlInterpreter->get_file_check_description();
+	},
+	'Retrieve the description.',
+);
+
+ok_string(
+	$file_check_description,
+	name        => 'The description',
+	allow_empty => 0,
+);

t/05-get_file_pattern.t
@@ -1,0 +1,28 @@
+#!/usr/bin/env perl
+
+=head1 DESCRIPTION
+
+Verify that the get_file_pattern() subroutine returns a pattern to match file names
+against.
+
+=cut
+
+use strict;
+use warnings;
+
+use Test::FailWarnings -allow_deps => 1;
+use Test::More tests => 2;
+use Test::Type;
+
+use App::GitHooks::Plugin::PerlInterpreter;
+
+
+can_ok(
+	'App::GitHooks::Plugin::PerlInterpreter',
+	'get_file_pattern',
+);
+
+ok_regex(
+	App::GitHooks::Plugin::PerlInterpreter->get_file_pattern(),
+	name => 'The return value of get_file_pattern()',
+);