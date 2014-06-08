38f89d1c6af838b0182369f788e797ae614640eb
Guillaume Aubert <aubertg@cpan.org>
2014-06-08 15:21:30 -0700

    Release version 1.0.3.

--- lib/App/GitHooks/Plugin/PerlInterpreter.pm
+++ lib/App/GitHooks/Plugin/PerlInterpreter.pm
@@ -27,11 +27,11 @@ other developers don't invoke the system Perl by mistake.
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 
 =head1 CONFIGURATION OPTIONS
